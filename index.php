c2fe3207ea56e28f7ff06b142ed712d33e9059bb
Agastiya S. Mohammad <egi@fun-dering.com>
2010-11-18 00:11:29 +0700

    ! refactor output functions to class

--- index.php
+++ index.php
@@ -9,6 +9,105 @@
 require_once('gdi.class.php');
 define('LF', "\r\n");
 
+interface output
+{
+	function out($data);
+}
+
+class csv implements output
+{
+	function out($result)
+	{
+		$ret = '';
+		$rows = count($result);
+		foreach ($result[0] as $column => $value)
+			$head .= ($head ? ',' : '') . $column;
+		$ret .= $head . LF;
+		foreach ($result as $rows)
+		{
+			$row = '';
+			foreach ($rows as $column => $value)
+				$row .= ($row ? ',' : '') . $value;
+			$ret .= $row . LF;
+		}
+		return $ret;
+	}
+}
+
+class html implements output
+{
+	function out($result)
+	{
+		$ret  = '<table>';
+		$ret .= '<tr>';
+		foreach ($result[0] as $column => $value)
+			$ret .= '<th>' . $column . '</th>';
+		$ret .= '</tr>';
+		foreach ($result as $rows)
+		{
+			$ret .= '<tr>';
+			foreach ($rows as $column => $value)
+				$ret .= '<td>' . $value . '</td>';
+			$ret .= '</tr>';
+		}
+		$ret .= '</table>';
+		return $ret;
+	}
+}
+
+class xml implements output
+{
+	/**
+	 * Array to XML
+	 */
+	private function array_to_xml(&$array)
+	{
+		$ret = '';
+		foreach ($array as $key => $value)
+		{
+			$keyName = is_numeric($key) ? 'elm' . $key : $key;
+			if (!is_array($value))
+			{
+				$ret .= sprintf('<%1$s>%2$s</%1$s>', $keyName, $value) . LF;
+			}
+			else
+			{
+				$ret .= sprintf('<%1$s>', $keyName) . LF;
+				$ret .= array_to_xml($value);
+				$ret .= sprintf('</%1$s>', $keyName) . LF;
+			}
+		}
+		return($ret);
+	}
+
+	/**
+	 * output XML
+	 */
+	function out($apiData)
+	{
+		$ret  = '<?xml version="1.0"?>' . LF;
+		$ret .= '<gdi status="1">' . LF;
+		$ret .= $this->array_to_xml($apiData);
+		$ret .= '</gdi>' . LF;
+		//header('Content-type: text/xml');
+		return($ret);
+	}
+}
+
+class json implements output
+{
+	/**
+	 * output JSON
+	 */
+	function out($apiData)
+	{
+		$data = array('gdi'=>$apiData);
+		$ret  = json_encode($data);
+		//header('Content-type: application/json');
+		return($ret);
+	}
+}
+
 $gdi = new gdi();
 $table_name = (isset($_GET['q'])) ? $_GET['q'] : 'wilayah';
 $result = $gdi->get_data('SELECT * FROM ' . $table_name . ' LIMIT 0, 50');
@@ -20,89 +119,8 @@ if (PEAR::isError($result))
 }
 else
 {
-	$output_fn = 'output_html';
-	if (isset($_GET['f']) && function_exists('output_'.$_GET['f']))
-		$output_fn = 'output_' . $_GET['f'];
-	echo $output_fn($result);
-}
-
-function output_csv($result)
-{
-	$ret = '';
-	$rows = count($result);
-	foreach ($result[0] as $column => $value)
-		$head .= ($head ? ',' : '') . $column;
-	$ret .= $head . LF;
-	foreach ($result as $rows)
-	{
-		$row = '';
-		foreach ($rows as $column => $value)
-			$row .= ($row ? ',' : '') . $value;
-		$ret .= $row . LF;
-	}
-	return $ret;
-}
-
-function output_html($result)
-{
-	$ret  = '<table>';
-	$ret .= '<tr>';
-	foreach ($result[0] as $column => $value)
-		$ret .= '<th>' . $column . '</th>';
-	$ret .= '</tr>';
-	foreach ($result as $rows)
-	{
-		$ret .= '<tr>';
-		foreach ($rows as $column => $value)
-			$ret .= '<td>' . $value . '</td>';
-		$ret .= '</tr>';
-	}
-	$ret .= '</table>';
-	return $ret;
-}
-
-/**
- * Array to XML
- */
-function array_to_xml(&$array)
-{
-	foreach ($array as $key => $value)
-	{
-		$keyName = is_numeric($key) ? 'elm' . $key : $key;
-		if (!is_array($value))
-		{
-			$ret .= sprintf('<%1$s>%2$s</%1$s>', $keyName, $value) . LF;
-		}
-		else
-		{
-			$ret .= sprintf('<%1$s>', $keyName) . LF;
-			$ret .= array_to_xml($value);
-			$ret .= sprintf('</%1$s>', $keyName) . LF;
-		}
-	}
-	return($ret);
-}
-
-/**
- * output XML
- */
-function output_xml(&$apiData)
-{
-	$ret .= '<?xml version="1.0"?>' . LF;
-	$ret .= '<gdi status="1">' . LF;
-	$ret .= array_to_xml($apiData);
-	$ret .= '</gdi>' . LF;
-	//header('Content-type: text/xml');
-	return($ret);
-}
-
-/**
- * output JSON
- */
-function output_json(&$apiData)
-{
-	$data = array('gdi'=>$apiData);
-	$ret .= json_encode($data);
-	//header('Content-type: application/json');
-	return($ret);
+	$output_class = 'html';
+	if (isset($_GET['f']) && class_exists($_GET['f'])) $output_class = $_GET['f'];
+	$o = new $output_class;
+	echo $o->out($result);
 }
